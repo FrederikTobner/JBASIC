5743480607a4548a782b80b4b50fe1cd7e7ec3b5
Frederik Tobner <masterhorst90@web.de>
2022-12-27 22:28:46 +0100

    ups

---
+++ docs/_function_arity_exception_8java.js
@@ -0,0 +1,4 @@
+var _function_arity_exception_8java =
+[
+    [ "org.jbasic.FunctionArityException", "classorg_1_1jbasic_1_1_function_arity_exception.html", "classorg_1_1jbasic_1_1_function_arity_exception" ]
+];
---
+++ docs/classorg_1_1jbasic_1_1_function_arity_exception.js
@@ -0,0 +1,4 @@
+var classorg_1_1jbasic_1_1_function_arity_exception =
+[
+    [ "FunctionArityException", "classorg_1_1jbasic_1_1_function_arity_exception.html#ae86f3e12e1c5c78e9f7883e78f852f16", null ]
+];
