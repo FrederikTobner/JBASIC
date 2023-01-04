130b8d30a6605edb8faa95e93e7cc11018e7f140
Frederik Tobner <masterhorst90@web.de>
2023-01-04 12:48:23 +0100

    cleanup

---
+++ docs/classorg_1_1jbasic_1_1core_1_1_i_o_formatter.js
@@ -0,0 +1,5 @@
+var classorg_1_1jbasic_1_1core_1_1_i_o_formatter =
+[
+    [ "formatErrorMessage", "classorg_1_1jbasic_1_1core_1_1_i_o_formatter.html#a1c9111789429491e9692128fb4b8d8e2", null ],
+    [ "numericalOutputFormat", "classorg_1_1jbasic_1_1core_1_1_i_o_formatter.html#ac6abea9e7e1f557edf51eb11f746430b", null ]
+];
---
+++ docs/classorg_1_1jbasic_1_1core_1_1_random_numbers_generator.js
@@ -0,0 +1,5 @@
+var classorg_1_1jbasic_1_1core_1_1_random_numbers_generator =
+[
+    [ "doubleRandomWithinRange", "classorg_1_1jbasic_1_1core_1_1_random_numbers_generator.html#a76cf93f7147f6f072c3ed5886805e34f", null ],
+    [ "randomNumberGenerator", "classorg_1_1jbasic_1_1core_1_1_random_numbers_generator.html#a56f093d7844b12a691188b11298a139f", null ]
+];
